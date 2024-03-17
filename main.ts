acee915b3f4bd698beeb76f43168e44a0813d8b1
Ilsa. <ilsasyeda@gmail.com>
2024-03-17 17:42:13 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,7 @@
+function city_country(city: string, country: string): string {
+    return `${city}: ${country}`
+}
+
+console.log(city_country("karachi", "pakistan"));
+console.log(city_country("islamabad", "pakistan"));
+console.log(city_country("paris", "france"));
